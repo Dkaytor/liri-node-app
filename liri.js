af374019d29b2ce96bfc0889508fb680dd37d5fe
Dina <counting1121@yahoo.com>
2019-01-26 09:42:38 -0500

    problem with default check

--- liri.js
+++ liri.js
@@ -3,32 +3,94 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
+var Spotify = require("node-spotify-api");
 
-//var command = process.argv[2];
-var artist = process.argv[2];
-
-
-axios.get("https://rest.bandsintown.com.artists/" + artist + "/events?app_id=codingbootcamp")
-.then(function(response) {
-  console.log(" The name of the venue is: " + response.data.venue.name);
-});
-
-    //return console.log("The location is: " + response.items.album.artists[3]);
-    //return console.log("The date is: " + response.items.album.artists[3]);
+var command = process.argv[2];
+var artist = process.argv[3];
+var spotify = new Spotify(keys.spotify);
 
 
-/*if (command === "concert-this") {
-  console.log(response);
-}
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+  .then(function (response) {
+    if (command === "concert-this") {
 
-else if (command === "spotify-this-song") {
+      for (i = 2; i < response.data.length; i++) {
+        console.log(" The name of the venue is: " + response.data[i].venue.name);
+        console.log(" The location is: " + response.data[i].venue.city);
+        console.log(" The date is: " + moment(response.data[i].datetime).format('MM DD YYYY'));
+        console.log("\n-----------------\n");
+      }
+    }
 
-}
+    else if (command === "spotify-this-song") {
+      if (artist == "") {
+        spotify.search({ type: 'track', query: 'The Sign' }, function (err, response) {
+          if (err) {
+            return console.log('Error occurred: ' + err);
+          }//ends if err
+                     
+           for (i = 2; i < response.tracks.items.length; i++) {
+            console.log(" The artist is: " + response.tracks.items[i].artists[0].name);
+            console.log(" The song is: " + response.tracks.items[i].name);
+            console.log(" The preview site is: " + response.tracks.items[i].external_urls.spotify);
+            console.log(" The album is: " + response.tracks.items[i].album.name);
+            console.log("\n-----------------\n");
+          }//ends loop
+        
+        });//ends spotify
+      }//ends if artist
 
-else if (command === "movie-this") {
+      spotify.search({ type: 'track', query: artist }, function (err, response) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }//ends if err
+                   
+         for (i = 2; i < response.tracks.items.length; i++) {
+          console.log(" The artist is: " + response.tracks.items[i].artists[0].name);
+          console.log(" The song is: " + response.tracks.items[i].name);
+          console.log(" The preview site is: " + response.tracks.items[i].external_urls.spotify);
+          console.log(" The album is: " + response.tracks.items[i].album.name);
+          console.log("\n-----------------\n");
+        }//end loop
+      
+      });//ends spotify
+    }//ends spotify command
 
-}
+    else if (command === "movie-this") {
+      if (artist = " ") {
+        axios.get("http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy")
+      .then( function(response) {        
+        console.log("The name of the movie is: " + response.data.Title);
+        console.log("The movie came out in " + response.data.Year);
+        console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+        console.log("The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value);
+        console.log(response.data.Title + " was made in: " + response.data.Country);
+        console.log(response.data.Title + " is in: " + response.data.Language);
+        console.log(response.data.Title + " is about " + response.data.Plot);
+        console.log("The actors are: " + response.data.Actors);
+        console.log("\n-----------------\n");        
+         })//ends .then function
+      } //ends if artist
+      
+     else { 
+      axios.get("http://www.omdbapi.com/?t=" + artist +"&y=&plot=short&apikey=trilogy")
+      .then( function(response) {
+        console.log("The name of the movie is: " + response.data.Title);
+        console.log("The movie came out in " + response.data.Year);
+        console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+        console.log("The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value);
+        console.log(response.data.Title + " was made in: " + response.data.Country);
+        console.log(response.data.Title + " is in: " + response.data.Language);
+        console.log(response.data.Title + " is about " + response.data.Plot);
+        console.log("The actors are: " + response.data.Actors);
+        console.log("\n-----------------\n");        
 
-else if (command === "do-what-it-says") {
+      })//ends .then
+    
+    }
+    }// ends movie command
 
-}*/
+    
+  });
+
+
